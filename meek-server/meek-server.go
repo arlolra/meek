5f3c515f502acc4d73b78024168b61454088e4d1
David Fifield <david@bamsoftware.com>
2014-01-26 18:32:31 -0800

    HTTP handling for server.

--- meek-server/meek-server.go
+++ meek-server/meek-server.go
@@ -5,15 +5,19 @@ import (
 	"io"
 	"log"
 	"net"
+	"net/http"
 	"os"
 	"os/signal"
-	"sync"
+	"path"
 	"syscall"
+	"time"
 )
 
 import "git.torproject.org/pluggable-transports/goptlib.git"
 
 const ptMethodName = "meek"
+const minSessionIdLength = 32
+const maxPayloadLength = 0x10000
 
 var ptInfo pt.ServerInfo
 
@@ -21,59 +25,116 @@ var ptInfo pt.ServerInfo
 // ends, -1 is written.
 var handlerChan = make(chan int)
 
-func copyLoop(a, b net.Conn) {
-	var wg sync.WaitGroup
-	wg.Add(2)
-
-	go func() {
-		io.Copy(b, a)
-		wg.Done()
-	}()
-	go func() {
-		io.Copy(a, b)
-		wg.Done()
-	}()
-
-	wg.Wait()
+func httpBadRequest(w http.ResponseWriter) {
+        http.Error(w, "Bad request.\n", http.StatusBadRequest)
 }
 
-func handler(conn net.Conn) error {
-	defer conn.Close()
+func httpInternalServerError(w http.ResponseWriter) {
+	http.Error(w, "Internal server error.\n", http.StatusInternalServerError)
+}
 
+type State struct {
+	sessionMap map[string]*net.TCPConn
+}
+
+func NewState() *State {
+	state := new(State)
+	state.sessionMap = make(map[string]*net.TCPConn)
+	return state
+}
+
+func (state *State) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	handlerChan <- 1
 	defer func() {
 		handlerChan <- -1
 	}()
 
-	or, err := pt.DialOr(&ptInfo, conn.RemoteAddr().String(), ptMethodName)
-	if err != nil {
-		return err
+	switch req.Method {
+	case "GET":
+		state.Get(w, req)
+	case "POST":
+		state.Post(w, req)
+	default:
+		httpBadRequest(w)
 	}
-	defer or.Close()
-
-	copyLoop(conn, or)
-
-	return nil
 }
 
-func acceptLoop(ln net.Listener) error {
-	defer ln.Close()
-	for {
-		conn, err := ln.Accept()
-		if err != nil {
-			log.Printf("error in Accept: %s", err)
-			if e, ok := err.(net.Error); ok && !e.Temporary() {
-				return err
-			}
-			continue
-		}
-		go func() {
-			err = handler(conn)
-			if err != nil {
-				log.Printf("error in handling connection: %s", err)
-			}
-		}()
+func (state *State) Get(w http.ResponseWriter, req *http.Request) {
+	if path.Clean(req.URL.Path) != "/" {
+		http.NotFound(w, req)
+		return
 	}
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("I’m just a happy little web server.\n"))
+}
+
+func (state *State) Post(w http.ResponseWriter, req *http.Request) {
+	var err error
+
+	id := req.Header.Get("x-session-id")
+	if len(id) < minSessionIdLength {
+		httpBadRequest(w)
+		return
+	}
+
+	or, ok := state.sessionMap[id]
+	if ok {
+		log.Printf("already existing session id %q", id)
+	} else {
+		log.Printf("unknown session id %q; creating new session", id)
+		or, err = pt.DialOr(&ptInfo, req.RemoteAddr, ptMethodName)
+		if err != nil {
+			log.Printf("error in DialOr: %s", err)
+			httpInternalServerError(w)
+			return
+		}
+		state.sessionMap[id] = or
+	}
+
+	body := http.MaxBytesReader(w, req.Body, maxPayloadLength)
+	nc, err := io.Copy(or, body)
+	if err != nil {
+		log.Printf("error copying body to ORPort: %s", err)
+		return
+	}
+	log.Printf("copied %d bytes to ORPort", nc)
+
+	buf := make([]byte, maxPayloadLength)
+	or.SetReadDeadline(time.Now().Add(10 * time.Millisecond))
+	n, err := or.Read(buf)
+	if err != nil {
+		if e, ok := err.(net.Error); !ok || !e.Timeout() {
+			log.Printf("error reading from ORPort: %s", err)
+			return
+		}
+	}
+	log.Printf("read %d bytes from ORPort: %q", n, buf[:n])
+	n, err = w.Write(buf[:n])
+	if err != nil {
+		log.Printf("error writing to response: %s", err)
+		return
+	}
+	log.Printf("wrote %d bytes to response", n)
+}
+
+func startListener(network string, addr *net.TCPAddr) (net.Listener, error) {
+	ln, err := net.ListenTCP(network, addr)
+	if err != nil {
+		return nil, err
+	}
+	state := NewState()
+	server := &http.Server{
+		Handler: state,
+	}
+	go func() {
+		defer ln.Close()
+		err = server.Serve(ln)
+		if err != nil {
+			log.Printf("Error in Serve: %s", err)
+		}
+	}()
+	return ln, nil
 }
 
 func main() {
@@ -107,12 +168,11 @@ func main() {
 		}
 		switch bindaddr.MethodName {
 		case ptMethodName:
-			ln, err := net.ListenTCP("tcp", bindaddr.Addr)
+			ln, err := startListener("tcp", bindaddr.Addr)
 			if err != nil {
 				pt.SmethodError(bindaddr.MethodName, err.Error())
 				break
 			}
-			go acceptLoop(ln)
 			pt.Smethod(bindaddr.MethodName, ln.Addr())
 			log.Printf("listening on %s", ln.Addr())
 			listeners = append(listeners, ln)
