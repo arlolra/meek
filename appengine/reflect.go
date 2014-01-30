f488b91224a9d644bfae86510ccf3734b8dddaa7
David Fifield <david@bamsoftware.com>
2014-01-30 11:38:07 -0800

    Whitelist response headers too.
    There was a problem where meek-server was setting Transfer-Encoding:
    chunked and App Engine was adding its own Content-Length header.

--- appengine/reflect.go
+++ appengine/reflect.go
@@ -21,11 +21,11 @@ func pathJoin(a, b string) string {
 	return a + b
 }
 
-// We reflect only a whitelisted set of header fields in requests. The full list
-// includes things like User-Agent and X-Appengine-Country that the Tor bridge
-// doesn't need to know. Responses, on the other hand, copy the full set of
-// headers.
-var reflectedRequestFields = []string{
+// We reflect only a whitelisted set of header fields. In requests, the full
+// list includes things like User-Agent and X-Appengine-Country that the Tor
+// bridge doesn't need to know. In responses, there may be things like
+// Transfer-Encoding that interfere with App Engine's own hop-by-hop headers.
+var reflectedHeaderFields = []string{
 	"Content-Type",
 	"X-Session-Id",
 }
@@ -41,7 +41,7 @@ func copyRequest(r *http.Request) (*http.Request, error) {
 	if err != nil {
 		return nil, err
 	}
-	for _, key := range reflectedRequestFields {
+	for _, key := range reflectedHeaderFields {
 		value := r.Header.Get(key)
 		if value != "" {
 			c.Header.Add(key, value)
@@ -62,8 +62,9 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	for key, values := range resp.Header {
-		for _, value := range values {
+	for _, key := range reflectedHeaderFields {
+		value := resp.Header.Get(key)
+		if value != "" {
 			w.Header().Add(key, value)
 		}
 	}
