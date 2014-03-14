f17d4ab980eae475dd4c025d5fe092046a1d65ac
David Fifield <david@bamsoftware.com>
2014-03-14 00:05:16 -0700

    Four-space indents.

--- firefox/components/main.js
+++ firefox/components/main.js
@@ -11,20 +11,20 @@ function MeekHTTPHelper() {
     // Create a "direct" nsIProxyInfo that bypasses the default proxy.
     // https://developer.mozilla.org/en-US/docs/XPCOM_Interface_Reference/nsIProtocolProxyService
     var pps = Components.classes["@mozilla.org/network/protocol-proxy-service;1"]
-            .getService(Components.interfaces.nsIProtocolProxyService);
+        .getService(Components.interfaces.nsIProtocolProxyService);
     // https://developer.mozilla.org/en-US/docs/XPCOM_Interface_Reference/nsIProxyInfo
     var proxy = pps.newProxyInfo("direct", "", 0, 0, 0xffffffff, null);
 
     // https://developer.mozilla.org/en-US/docs/XPCOM_Interface_Reference/nsIIOService
     var ioService = Components.classes["@mozilla.org/network/io-service;1"]
-            .getService(Components.interfaces.nsIIOService);
+        .getService(Components.interfaces.nsIIOService);
     var httpProtocolHandler = ioService.getProtocolHandler("http")
-            .QueryInterface(Components.interfaces.nsIHttpProtocolHandler);
+        .QueryInterface(Components.interfaces.nsIHttpProtocolHandler);
     var uri = ioService.newURI(FRONT_URL, null, null);
     // Construct an HTTP channel with the proxy bypass.
     // https://developer.mozilla.org/en-US/docs/XPCOM_Interface_Reference/nsIHttpChannel
     var channel = httpProtocolHandler.newProxiedChannel(uri, proxy, 0, null)
-            .QueryInterface(Components.interfaces.nsIHttpChannel);
+        .QueryInterface(Components.interfaces.nsIHttpChannel);
     // Set the host we really want.
     channel.setRequestHeader("Host", HOST, false);
     channel.redirectionLimit = 0;
@@ -33,21 +33,21 @@ function MeekHTTPHelper() {
 
     // https://developer.mozilla.org/en-US/docs/Creating_Sandboxed_HTTP_Connections
     function StreamListener() {
-            this.onStartRequest = function(aRequest, aContext) {
-                    dump("onStartRequest\n");
-            };
-            this.onStopRequest = function(aRequest, aContext, aStatus) {
-                    dump("onStopRequest\n");
-            };
-            this.onDataAvailable = function(aRequest, aContext, aStream, aSourceOffset, aLength) {
-                    dump("onDataAvailable\n");
-                    var a = new Uint8Array(aLength);
-                    var input = Components.classes["@mozilla.org/binaryinputstream;1"]
-                            .createInstance(Components.interfaces.nsIBinaryInputStream);
-                    input.setInputStream(aStream);
-                    input.readByteArray(aLength, a);
-                    dump(aLength + ":" + a + "\n");
-            };
+        this.onStartRequest = function(aRequest, aContext) {
+            dump("onStartRequest\n");
+        };
+        this.onStopRequest = function(aRequest, aContext, aStatus) {
+            dump("onStopRequest\n");
+        };
+        this.onDataAvailable = function(aRequest, aContext, aStream, aSourceOffset, aLength) {
+            dump("onDataAvailable\n");
+            var a = new Uint8Array(aLength);
+            var input = Components.classes["@mozilla.org/binaryinputstream;1"]
+                .createInstance(Components.interfaces.nsIBinaryInputStream);
+            input.setInputStream(aStream);
+            input.readByteArray(aLength, a);
+            dump(aLength + ":" + a + "\n");
+        };
     }
 
     var listener = new StreamListener();
