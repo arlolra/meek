9324923fb437ca023de5d02d8b78fadc2aa27fbf
David Fifield <david@bamsoftware.com>
2014-03-18 12:19:02 -0700

    Oops, misedited URL.

--- appengine/reflect.go
+++ appengine/reflect.go
@@ -10,7 +10,7 @@ import (
 	"appengine/urlfetch"
 )
 
-const forwardURL = "http://meek.bamsoftware.com:7443/"
+const forwardURL = "http://meek.bamsoftware.com:7002/"
 // A timeout of 0 means to use the App Engine default (5 seconds).
 const urlFetchTimeout = 10 * time.Second
 var context appengine.Context
